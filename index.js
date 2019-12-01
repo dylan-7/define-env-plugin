aee2bf3535021bcdbfeeb0ebd62a8d9a3b31f73c
dylan-7 <wuxiewang@gmail.com>
2019-12-01 19:21:47 +0800

    Fixed:  🛠return object

--- index.js
+++ index.js
@@ -7,28 +7,28 @@ var webpack = require('webpack');
 function crossFields(keys) {
   var definitions = {};
 
-  if (!Array.isArray(keys)) {
+  if (keys && !Array.isArray(keys)) {
     console.log('❌ define-env-plugin must pass in an array');
     return definitions;
   }
 
   var keys = ['NODE_ENV', 'SERVICE_URL'].concat(keys);
 
-  if (keys.length !== 0) {
-    keys.forEach(function(item, index) {
-      if (process.env[item]) {
-        definitions[key] = JSON.stringify(process.env[item]);
-      } else {
-        console.log('⚠️ ' + item + ' is undefined in scripts');
-      }
-    });
-  }
+  keys.forEach(function(item, index) {
+    if (process.env[item]) {
+      definitions[item] = JSON.stringify(process.env[item]);
+    } else if (item !== undefined) {
+      console.log('⚠️ ' + item + ' is undefined in scripts');
+    }
+  });
   return definitions;
 }
 
 // keys: array ['a', 'b']
 function DefineEnvPlugin(keys) {
-  return new webpack.DefinePlugin(crossFields(keys));
+  return new webpack.DefinePlugin({
+    'process.env': crossFields(keys)
+  });
 }
 
 module.exports = DefineEnvPlugin;
